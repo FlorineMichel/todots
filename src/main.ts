434f5c27f966b611e2a0dccfc2feb243ac95aacd
Florine Michel <florinemichel@orange.fr>
2021-04-20 15:38:20 +0200

    change port listener

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ import { AppModule } from './todo.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.enableCors();
-  await app.listen(3000);
+  await app.listen(8080);
 }
 bootstrap();
